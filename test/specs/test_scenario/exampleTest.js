21c35041781c156c4539284b53984cb82bd373f4
pyavchik <pyavchik@gmail.com>
2018-12-04 23:11:50 +0200

    added ta locators

--- test/specs/test_scenario/exampleTest.js
+++ test/specs/test_scenario/exampleTest.js
@@ -1,10 +1,10 @@
+const ta = require('trueautomation-helper').ta;
+
 describe('TrueAutomation.IO page', ()=> {
     it('should set email to email field', ()=> {
         browser.url('https://trueautomation.io');
-        browser.click('a.login-btn');
-        browser.waitForExist('div.sign-up-container > a');
-        browser.click('div.sign-up-container > a');
-        browser.waitForExist('input[name="email"]');
-        browser.setValue('input[name="email"]', 'your@gmail.com');
+        browser.click(ta('loginBtn', '.login-btn'));
+        browser.click(ta('signUpBtn', '.sign-up-container a'));
+        browser.setValue(ta('emailFld', '*[name="email"]'), 'your@gmail.com');
     });
 });
